d4d2c820e196e1282ac94e41a981951013aa7134
pratikaman <pratikaman.atom@gmail.com>
2023-02-07 17:53:07 +0530

    more changes

--- 4_fun_facts_no_expressjs/src/components/Navbar.js
+++ 4_fun_facts_no_expressjs/src/components/Navbar.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React from "react";
 
-export default function Navbar (){
-    return (
-        <h1>Navbar component</h1>
-    )
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src="images/react-icon-small.png"
+        alt="react icon"
+        className="nav--icon"
+      />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
