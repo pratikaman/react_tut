3c3de645855328945407ad8319bdf09db2745de7
pratikaman <pratikaman.atom@gmail.com>
2023-02-07 16:32:44 +0530

    Refactor

---
+++ 4_fun_facts_no_expressjs/src/App.js
@@ -0,0 +1,16 @@
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
+import React from "react"
+
+export default function App(){
+return (
+    <div className="container">
+        <Navbar />
+        <Main />
+    </div>
+)
+
+}
+
+
+
