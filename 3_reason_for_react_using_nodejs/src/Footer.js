60c8581af11175f8e7154158c61c33f463f9cda8
pratikaman <pratikaman.atom@gmail.com>
2023-02-07 15:28:37 +0530

    Some changes

--- 3_reason_for_react_using_nodejs/src/Footer.js
+++ 3_reason_for_react_using_nodejs/src/Footer.js
@@ -1,7 +1,11 @@
-export default function Footer() {
+import React from "react"
+
+function Footer() {
     return (
       <footer>
         <small>&copy; Pratik development. All rights reserved.</small>
       </footer>
     );
   }
+
+  export {Footer}
---
+++ 3_reason_for_react_using_nodejs/src/index.js
@@ -0,0 +1,89 @@
+/// using default import
+import Header from "./Header";
+import Main from "./Main";
+/// or using named import
+import {Footer} from "./Footer"
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+/// **********************************************************************************************
+/// Practice example
+// const funFact = (
+//     <div>
+//     <img src="./react-logo.png" alt="react logo" width="40px"/>
+//     <h1>Fun Facts about React</h1>
+//     <ul>
+//         <li>Was first released in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Has well over 100K stars on github</li>
+//         <li>Is maintained by Facebook</li>
+//         <li>Powers thousands of enterprise app, including mobile apps</li>
+//     </ul>
+//     </div>
+
+// )
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// root.render(funFact)
+/// ********************************************************************************************
+
+
+/// ********************************************************************************************
+
+/// These are shifted to separate files thats why commented.
+// function Header() {
+//     return (
+//       <header>
+//         <nav class="nav">
+//           <img src="./react-logo.png" alt="react" className="nav-logo" />
+//           <ul className="nav-items">
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//           </ul>
+//         </nav>
+//       </header>
+//     );
+//   }
+
+// function Footer() {
+//   return (
+//     <footer>
+//       <small>&copy; Pratik development. All rights reserved.</small>
+//     </footer>
+//   );
+// }
+
+// function Main() {
+//   return (
+//     <div className="main">
+//       <h1>Reasons I'm excited to learn react</h1>
+//       <ol className="reasons">
+//         <li>Its a popular library. So its cool to learn it</li>
+//         <li>More likely to get me a job</li>
+//       </ol>
+//     </div>
+//   );
+// }
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+
+      {/* or uncomment and use below code */}
+      {/* {Header()} */}
+      {/* {Main()} */}
+      {/* {Footer()} */}
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Page />)
+// or
+root.render(Page());
