740b1dce31086ece46385cbadfd68322e2d2a34d
pratikaman <pratikaman.atom@gmail.com>
2023-02-11 19:44:31 +0530

    Some more changes

--- dynamic_projects/4_meme_generator/src/components/Meme.js
+++ dynamic_projects/4_meme_generator/src/components/Meme.js
@@ -3,19 +3,40 @@ import {memes} from "../memesData"
 
 export default function Meme() {
 
-    const handleOnClick = ()=>{
-       const memesList = memes.data.memes;
-       let randomIndex = Math.floor(Math.random()*memesList.length)
-       return alert(memesList[randomIndex].url)
-    }
+
+    /// database state
+    const [allMemeImages, changeMemeDatabase] = React.useState(memes)
+
+    ///  meme state
+    const [meme, changeMeme] = React.useState({
+      topText:"",
+      bottomText: "",
+      randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function getMemeImage() {
+      const memesArray = memes.data.memes
+      const randomNumber = Math.floor(Math.random() * memesArray.length)
+      changeMeme((prevMeme)=>{
+        return {
+          ...prevMeme,
+          randomImage: allMemeImages.data.memes[randomNumber].url
+        }
+      })
+      
+  }
+
+     
   return (
     <main>
       <div action="" className="form">
         <input type="text" placeholder="Top Text" className="form--input" />
         <input type="text" placeholder="Bottom Text" className="form--input" />
 
-        <button className="form--button" onClick={handleOnClick}>Get a new meme image 🖼</button>
+        <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
+
+      <img className="meme--image" src={meme.randomImage} alt="" />
     </main>
   );
 }
